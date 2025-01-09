35b2d231ebff67ea6f5c2f5749b4bdab0fa77451
Jordon-py <c.jordon.icloud.com>
2025-01-09 08:12:19 -0800

    updated script.js added function declarations

---
+++ script.js
@@ -0,0 +1,63 @@
+```My choice of game
+    **Spaceman**
+"Spaceman" is a simple guessing game similar to "Hangman." Players guess letters to uncover a hidden word, and each incorrect guess progresses a visual representation of a spaceman until it's fully drawn (representing failure).
+
+---
+
+- *Spaceman_Reloaded*
+
+Psuedocode
+- 1. Create Game Variables:
+
+     - let listName = []     //create empty list
+     
+
+
+- 2. Display Instructions:
+
+- 3. Main Game Loop:
+
+- 4. End of Game:
+```
+
+/*-------------- Constants -------------*/
+const spaceList = [
+    Planets, //(7 letters)
+    Asteroid, // (8 letters)
+    Universe, // (8 letters),
+    Galactic, // (8 letters),
+    Darkmatter, // (10 letters),
+    Starfield, // (9 letters),
+    Spacewalk, // (9 letters),
+    Telescope,]; // (9 letters),
+
+
+/*---------- Variables (state) ---------*/
+let gameWord = ''
+let timer = ''
+let player = ''
+
+/*----- Cached Element References  -----*/
+
+
+/*-------------- Functions -------------*/
+function initializeGame() {
+    // randomly select a word, reset guesses, update UI,
+    player.querySelector()
+
+  }
+  
+  function handleGuess() {
+    //  check if 'letter' is in currentWord, handle correctness or penalty.
+    //  (timer). -10 sec
+  }
+  
+  function checkWinCondition() {
+      //  if all letters are revealed, declare victory.
+      
+  }
+  
+  function checkLoseCondition() {
+    //  if guessesRemaining <= 0 or the timer hits 0.
+
+/*----------- Event Listeners ----------*/
